d5c51b001e3e09bf3febec1329b1485ab2bb5108
dciccale <539546+dciccale@users.noreply.github.com>
2025-09-25 22:09:43 +0100

    update conf

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,13 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async rewrites() {
     const appOrigin = process.env.APP_ORIGIN || "http://localhost:3001";
-    return [
+    console.log("appOrigin", appOrigin);
+    const rewrites = [
       // Rewrite app routes
       { source: "/app", destination: `${appOrigin}` },
       { source: "/app/:path*", destination: `${appOrigin}/:path*` },
       // Forward API routes to app deployment
       { source: "/api/:path*", destination: `${appOrigin}/api/:path*` },
     ];
+    console.log("rewrites", rewrites);
+    return rewrites;
   },
 };
 
